556a5564c3a83bcd1f48368eca308742a3bb6b31
Frost15777 <di.yu.mk@gmail.com>
2024-01-30 22:50:19 +0100

    main

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { ContactItems, ContactName, ContactNumber, Button } from './ContactItem.styled'
+
+function ContactItem({ contact, deleteContact }) {
+  const handleDelete = () => {
+    deleteContact(contact.id);
+  };
+
+  return (
+    <ContactItems>
+      <ContactName>{contact.name}</ContactName>
+      <ContactNumber>{contact.number}</ContactNumber>
+      <Button onClick={handleDelete}>Delete</Button>
+    </ContactItems>
+  );
+}
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactItem
